2dc814ed242105469c5ec6a901e5e632b9dcbf78
Nikhilg0208 <nikhilg1098@gmail.com>
2025-07-23 16:07:17 +0530

    chage added

--- src/app/components/Viewport.tsx
+++ src/app/components/Viewport.tsx
@@ -6,7 +6,7 @@ const Viewport = forwardRef<HTMLDivElement>((props, ref) => {
     <div className="flex justify-center mt-6">
       <div
         ref={ref}
-        className="w-[700px] h-[700px] border border-gray-300 bg-black"
+        className="w-[900px] h-[650px] border border-gray-300 bg-black"
       />
     </div>
   );
